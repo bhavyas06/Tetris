6bdb2aeac936f1e406c295e0a59356b3193e4875
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-16 19:13:16 +0530

    Created drawBoard function to draw the grid filled with vacant sqaures

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const SQ = 20;
 
+const Row = 20;
+const Col = 10;
+const Vacant = "white";
+
 const cvs = document.getElementById('tetris');
 const ctx = cvs.getContext('2d');
 
@@ -8,4 +12,19 @@ function drawSquare(x, y, color) {
     ctx.fillRect(x*SQ, y*SQ, SQ, SQ);
     ctx.strokeStyle = "black";
     ctx.strokeRect(x*SQ, y*SQ, SQ, SQ);
+}
+
+let board = [];
+
+for(r=0; r<Row; r++) {
+    board[r] = [];
+    for(c=0; c<Col; c++) 
+        board[r][c] = Vacant;
+}
+
+function drawBoard() {
+    for(r=0; r<Row; r++) {
+        for(c=0; c<Col; c++)
+            drawSquare(c, r, board[r][c]);
+    }
 }
