75e6f1d97c6f19f531e623b0b03710a36f5f64e3
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-17 19:04:53 +0530

    added event listners to move the pieces using arrow keys

--- index.js
+++ index.js
@@ -69,3 +69,15 @@ Piece.prototype.unDraw = function() {
         }
     }
 }
+
+document.addEventListener("keydown", CONTROL);
+function CONTROL(event) {
+    if(event.keyCode == 37)
+        piece.moveLeft();
+    else if(event.keyCode == 38)
+        piece.rotate();
+    else if(event.keyCode == 39)
+        piece.moveRight();
+    else if(event.keyCode == 40)
+        piece.moveDown();
+}
