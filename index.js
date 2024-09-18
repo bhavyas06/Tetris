7c049dce2834b6051bca8a995cccba8916822f18
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-18 23:09:14 +0530

    now, removing the rows which are full and adding new row at the top, also updating the game score.

--- index.js
+++ index.js
@@ -214,28 +214,28 @@ Piece.prototype.lock = function(){
             board[this.y+r][this.x+c] = this.color;
         }
     }
-
-    // // remove full rows
-    // for(r = 0; r < ROW; r++){
-    //     let isRowFull = true;
-    //     for( c = 0; c < COL; c++){
-    //         isRowFull = isRowFull && (board[r][c] != VACANT);
-    //     }
-    //     if(isRowFull){
-    //         // if the row is full
-    //         // we move down all the rows above it
-    //         for( y = r; y > 1; y--){
-    //             for( c = 0; c < COL; c++){
-    //                 board[y][c] = board[y-1][c];
-    //             }
-    //         }
-    //         // the top row board[0][..] has no row above it
-    //         for( c = 0; c < COL; c++){
-    //             board[0][c] = VACANT;
-    //         }
-    //         // increment the score
-    //         score += 10;
-    //     }
+    
+    // remove full rows
+    for(r = 0; r < ROW; r++){
+        let isRowFull = true;
+        for( c = 0; c < COL; c++){
+            isRowFull = isRowFull && (board[r][c] != VACANT);
+        }
+        if(isRowFull){
+            // if the row is full
+            // we move down all the rows above it
+            for( y = r; y > 1; y--){
+                for( c = 0; c < COL; c++){
+                    board[y][c] = board[y-1][c];
+                }
+            }
+            // the top row board[0][..] has no row above it
+            for( c = 0; c < COL; c++){
+                board[0][c] = VACANT;
+            }
+            // increment the score
+            score += 10;
+        }
     }
     // update the board
     drawBoard();
