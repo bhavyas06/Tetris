0d9c9dadf47e34a9721b3a55158ca26a3192e40b
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-18 23:06:19 +0530

    created the lock function which locks the tetrominoes and changes the color of the grid

--- index.js
+++ index.js
@@ -176,3 +176,70 @@ Piece.prototype.collision = function(x,y,piece){
     }
     return false;
 }
+
+// CONTROL the piece
+document.addEventListener("keydown",CONTROL);
+function CONTROL(event){
+    if(event.keyCode == 37){
+        p.moveLeft();
+        dropStart = Date.now();
+    }else if(event.keyCode == 38){
+        p.rotate();
+        dropStart = Date.now();
+    }else if(event.keyCode == 39){
+        p.moveRight();
+        dropStart = Date.now();
+    }else if(event.keyCode == 40){
+        p.moveDown();
+    }
+}
+
+let score = 0;
+
+Piece.prototype.lock = function(){
+    for( r = 0; r < this.activeTetromino.length; r++){
+        for(c = 0; c < this.activeTetromino.length; c++){
+            // we skip the vacant squares
+            if( !this.activeTetromino[r][c]){
+                continue;
+            }
+            // pieces to lock on top = game over
+            if(this.y + r < 0){
+                alert("Game Over");
+                // stop request animation frame
+                gameOver = true;
+                break;
+            }
+            // we lock the piece
+            board[this.y+r][this.x+c] = this.color;
+        }
+    }
+
+    // // remove full rows
+    // for(r = 0; r < ROW; r++){
+    //     let isRowFull = true;
+    //     for( c = 0; c < COL; c++){
+    //         isRowFull = isRowFull && (board[r][c] != VACANT);
+    //     }
+    //     if(isRowFull){
+    //         // if the row is full
+    //         // we move down all the rows above it
+    //         for( y = r; y > 1; y--){
+    //             for( c = 0; c < COL; c++){
+    //                 board[y][c] = board[y-1][c];
+    //             }
+    //         }
+    //         // the top row board[0][..] has no row above it
+    //         for( c = 0; c < COL; c++){
+    //             board[0][c] = VACANT;
+    //         }
+    //         // increment the score
+    //         score += 10;
+    //     }
+    }
+    // update the board
+    drawBoard();
+    
+    // update the score
+    scoreElement.innerHTML = score;
+}
